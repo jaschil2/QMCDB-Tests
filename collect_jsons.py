b1696e5fe8483a20e7047bf76b6388904f42881c
Joshua Schiller <joshuaSchiller@wirelessprv-10-195-201-207.near.illinois.edu>
2016-05-17 12:54:04 -0500

    added more autogen runs

--- collect_jsons.py
+++ collect_jsons.py
@@ -1,15 +1,26 @@
 import json
 import os
-
+from pymatgen.io.cif import CifParser
 
 def add_jsons(directory,j_list):
 	for d in os.listdir(directory):
 		if os.path.isfile(directory+'/'+d) and 'json' in d and d.split('.')[1] == 'json':
-			j_list.append(json.load(open(directory+'/'+d,'r')))
-		elif os.path.isdir(d):
+			ext = json.load(open(directory+'/'+d,'r'))
+			if type(ext) == list:
+				j_list.extend(ext)
+			elif type(ext) == dict:
+				j_list.append(ext)
+			else:
+				print('Error, JSON file is of type: ' + str(type(ext)))
+ 		elif os.path.isdir(d):
 			j_list = add_jsons(os.getcwd()+'/'+d,j_list)
 	return j_list
 
 j_list  = []
 j_list = add_jsons(os.getcwd(),j_list)
+for d in j_list:
+	cif = d['cif']
+	parser=CifParser.from_string(cif)
+	struct=parser.get_structures()[0]
+	print(struct.formula)
 json.dump(j_list,open('all_files.json','w'))
